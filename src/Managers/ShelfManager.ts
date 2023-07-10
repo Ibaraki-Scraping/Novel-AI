5b170ca3938fdffe590a23ef29e12e0cda62e751
Ibaraki Douji <daynesmorgan33@gmail.com>
2023-07-10 16:06:36 +0200

    Refacto: removed data sub object

--- src/Managers/ShelfManager.ts
+++ src/Managers/ShelfManager.ts
@@ -69,38 +69,35 @@ export class ShelfManager {
     }
 
     public async create(options: {
-        data: {
-            storyMetadataVersion?: number,
-            id?: string,
-            title: string,
-            description?: string,
-            textPreview?: string,
-            isTA?: boolean,
-            favorite?: boolean,
-            tags?: string[],
-            createdAt?: number,
-            lastUpdatedAt?: number,
-            idModified?: boolean
-        }
+        storyMetadataVersion?: number,
+        id?: string,
+        title: string,
+        description?: string,
+        textPreview?: string,
+        isTA?: boolean,
+        favorite?: boolean,
+        tags?: string[],
+        createdAt?: number,
+        lastUpdatedAt?: number,
+        idModified?: boolean
     }): Promise<Awaited<ReturnType<typeof this.get>>[0]> {
 
-        if (!options.data.storyMetadataVersion) options.data.storyMetadataVersion = 1;
-        if (!options.data.id) options.data.id = v4();
-        if (!options.data.description) options.data.description = "";
-        if (!options.data.textPreview) options.data.textPreview = "";
-        if (!options.data.isTA) options.data.isTA = false;
-        if (!options.data.favorite) options.data.favorite = false;
-        if (!options.data.tags) options.data.tags = [];
-        options.data.createdAt = Date.now();
-        options.data.lastUpdatedAt = Date.now();
-        options.data.idModified = false;
+        if (!options.storyMetadataVersion) options.storyMetadataVersion = 1;
+        if (!options.id) options.id = v4();
+        if (!options.description) options.description = "";
+        if (!options.textPreview) options.textPreview = "";
+        if (!options.isTA) options.isTA = false;
+        if (!options.favorite) options.favorite = false;
+        if (!options.tags) options.tags = [];
+        options.createdAt = Date.now();
+        options.lastUpdatedAt = Date.now();
+        options.idModified = false;
 
         const res = JSON.parse((await this.ai['fetch']('/user/objects/shelf', {
             method: 'PUT'
         }, {
-            ...options,
             meta: v4(),
-            data: Buffer.from(JSON.stringify(options.data)).toString('base64')
+            data: Buffer.from(JSON.stringify(options)).toString('base64')
         })).body.toString());
         return await this.get(res.id);
     }
@@ -108,16 +105,16 @@ export class ShelfManager {
     public async update(id: string, options: Parameters<typeof this.create>[0]): Promise<Awaited<ReturnType<typeof this.get>>[0]> {
         const obj = await this.get(id);
 
-        if (!options.data.storyMetadataVersion) options.data.storyMetadataVersion = obj.data.storyMetadataVersion;
-        if (!options.data.id) options.data.id = obj.data.id;
-        if (!options.data.description) options.data.description = obj.data.description;
-        if (!options.data.textPreview) options.data.textPreview = obj.data.textPreview;
-        if (!options.data.isTA) options.data.isTA = obj.data.isTA;
-        if (!options.data.favorite) options.data.favorite = obj.data.favorite;
-        if (!options.data.tags) options.data.tags = obj.data.tags;
-        options.data.createdAt = obj.data.createdAt;
-        options.data.lastUpdatedAt = Date.now();
-        options.data.idModified = obj.data.idModified;
+        if (!options.storyMetadataVersion) options.storyMetadataVersion = obj.data.storyMetadataVersion;
+        if (!options.id) options.id = obj.data.id;
+        if (!options.description) options.description = obj.data.description;
+        if (!options.textPreview) options.textPreview = obj.data.textPreview;
+        if (!options.isTA) options.isTA = obj.data.isTA;
+        if (!options.favorite) options.favorite = obj.data.favorite;
+        if (!options.tags) options.tags = obj.data.tags;
+        options.createdAt = obj.data.createdAt;
+        options.lastUpdatedAt = Date.now();
+        options.idModified = obj.data.idModified;
 
         await this.ai['fetch']('/user/objects/shelf/' + id, {
             method: 'PATCH'
